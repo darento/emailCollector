5cdb43dc41ce0b1d45bb45e12b1baeb5bbf062b7
darento <dsartiom@gmail.com>
2024-01-28 16:48:12 +0100

    Added functions to extract expenses per month and per item.

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,25 @@
+from collections import defaultdict
+
+
 def convert_to_float(value: str) -> float:
     try:
         return float(value)
     except ValueError:
         return float(value.replace(",", "."))
+
+
+def extract_expenses_per_month(all_items):
+    expenses_per_month = defaultdict(float)
+    for items, date in all_items:
+        for item in items:
+            year, month = date[:4], date[4:6]
+            expenses_per_month[(year, month)] += item["total_price"]
+    return expenses_per_month
+
+
+def extract_expenses_per_item(all_items):
+    expenses_per_item = defaultdict(float)
+    for items, _ in all_items:
+        for item in items:
+            expenses_per_item[item["product"]] += item["total_price"]
+    return expenses_per_item
